2ef417572aab9109f75673b43aa24ccdcff73cac
Mark Raynsford <code@io7m.com>
2018-12-22 10:29:56 +0000

    Add Assimp module

---
+++ com.io7m.lwjgl.examples.assimp/src/main/java/com/io7m/lwjgl/examples/assimp/AssimpComponent.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2018 Mark Raynsford <code@io7m.com> http://io7m.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.io7m.lwjgl.examples.assimp;
+
+import org.lwjgl.assimp.AIScene;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.logging.Logger;
+
+/**
+ * An example Assimp component.
+ */
+
+@Component(immediate = true)
+public final class AssimpComponent
+{
+  private static final Logger LOG =
+    Logger.getLogger(AssimpComponent.class.getCanonicalName());
+
+  /**
+   * Construct a component.
+   */
+
+  public AssimpComponent()
+  {
+
+  }
+
+  /**
+   * Called by the OSGi runtime when the component is activated.
+   */
+
+  @Activate
+  public void onActivate()
+  {
+    LOG.info("onActivate");
+
+    LOG.info("allocating");
+    try (var scene = AIScene.calloc()) {
+      LOG.info("deallocating");
+    }
+  }
+}
